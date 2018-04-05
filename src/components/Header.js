93e8171fe6423546eba6e2aab71e23ab3e3d836f
Diego Santos <diego.santos@olxbr.com>
2018-04-05 20:46:16 -0300

    Add prop-types dependency

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Header(props) {
-    const title = props.title
-    return (
-        <div className="list-books-title">
-            <h1>{title}</h1>
-        </div>
-    )
+const Header = ({ title }) => (
+    <div className="list-books-title">
+        <h1>{title}</h1>
+    </div>
+)
+
+Header.PropTypes = {
+    title: PropTypes.string
 }
 
 export default Header
