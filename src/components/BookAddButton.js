d727cd09d1574dbedc6d36e76531eab2add6738a
Diego Santos <diego.santos@olxbr.com>
2018-04-07 11:11:38 -0300

    Route main and search pages
    It also adds a link in the main page to the search page.

---
+++ src/components/BookAddButton.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const BookAddButton = () => (<div className="open-search"><Link to="/search">Add a book</Link></div>)
+
+export default BookAddButton
