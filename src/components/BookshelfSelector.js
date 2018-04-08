e44eace9ab6884e73c48eb5b0aaa3e0759bea39a
Diego Santos <diego.santos@olxbr.com>
2018-04-08 13:11:05 -0300

    Disable bookshelf selector component on change event

--- src/components/BookshelfSelector.js
+++ src/components/BookshelfSelector.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import '../styles/BookshelfSelector.css'
 
-const shelfOptions = [
-    { title: 'Currently Reading', value: 'currentlyReading' },
-    { title: 'Want to Read', value: 'wantToRead' },
-    { title: 'Read', value: 'read' },
-    { title: 'None', value: null }
-]
+class BookshelfSelector extends Component {
 
-const BookshelfSelector = ({ shelf }) => (
-    <div className="book-shelf-changer">
-        <select value={shelf}>
-            <option value='placeholder' disabled>Move to...</option>
-            {shelfOptions.map(o => <option key={o.value} value={o.value}>{o.title}</option>)}
-        </select>
-    </div>
-)
+    static propTypes = {
+        shelf: PropTypes.string.isRequired
+    }
 
-BookshelfSelector.PropTypes = {
-    shelf: PropTypes.string.isRequired
+    state = {
+        disabled: false
+    }
+
+    shelfOptions = [
+        { title: 'Currently Reading', value: 'currentlyReading' },
+        { title: 'Want to Read', value: 'wantToRead' },
+        { title: 'Read', value: 'read' },
+        { title: 'None', value: null }
+    ]
+
+    onSelect = (value) => {
+        if (this.shelfOptions.find(function (o) { return o.value === value })) {
+            this.disable()
+        }
+    }
+
+    disable = () => {
+        this.setState({ disabled: true })
+    }
+
+    render() {
+        const { shelf } = this.props
+        return (
+            <div className="book-shelf-changer">
+                <select onChange={(event) => this.onSelect(event.target.value)} value={shelf} disabled={this.state.disabled}>
+                    <option key='placeholder' value='placeholder' disabled>Move to...</option>
+                    {this.shelfOptions.map(o => <option key={o.value} value={o.value}>{o.title}</option>)}
+                </select>
+            </div>
+        )
+    }
+
 }
 
 export default BookshelfSelector
